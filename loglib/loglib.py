145b2451a30cc4057bf38d10e014f7c8f1b65aa2
ppcrong <ppcrong@gmail.com>
2021-08-23 18:32:39 +0800

    [loglib] add ENUM_TEXT_COLOR and correct d1/i1/w1/e1/c1 apis

--- loglib/loglib.py
+++ loglib/loglib.py
@@ -1,9 +1,59 @@
 import datetime
+import enum
 import logging
 import os
 import sys
 
 from loglib.printlib import printlib
+
+
+class ENUM_TEXT_COLOR(enum.Enum):
+    """
+    ref: http://puremonkey2010.blogspot.com/2018/07/python-print-in-terminal-with-colors.html
+    """
+    END = '\33[0m'
+    BOLD = '\33[1m'
+    ITALIC = '\33[3m'
+    URL = '\33[4m'
+    BLINK = '\33[5m'
+    BLINK2 = '\33[6m'
+    SELECTED = '\33[7m'
+
+    BLACK = '\33[30m'
+    RED = '\33[31m'
+    GREEN = '\33[32m'
+    YELLOW = '\33[33m'
+    BLUE = '\33[34m'
+    VIOLET = '\33[35m'
+    BEIGE = '\33[36m'
+    WHITE = '\33[37m'
+
+    BLACKBG = '\33[40m'
+    REDBG = '\33[41m'
+    GREENBG = '\33[42m'
+    YELLOWBG = '\33[43m'
+    BLUEBG = '\33[44m'
+    VIOLETBG = '\33[45m'
+    BEIGEBG = '\33[46m'
+    WHITEBG = '\33[47m'
+
+    GREY = '\33[90m'
+    RED2 = '\33[91m'
+    GREEN2 = '\33[92m'
+    YELLOW2 = '\33[93m'
+    BLUE2 = '\33[94m'
+    VIOLET2 = '\33[95m'
+    BEIGE2 = '\33[96m'
+    WHITE2 = '\33[97m'
+
+    GREYBG = '\33[100m'
+    REDBG2 = '\33[101m'
+    GREENBG2 = '\33[102m'
+    YELLOWBG2 = '\33[103m'
+    BLUEBG2 = '\33[104m'
+    VIOLETBG2 = '\33[105m'
+    BEIGEBG2 = '\33[106m'
+    WHITEBG2 = '\33[107m'
 
 
 class loglib:
@@ -141,22 +191,22 @@ class loglib:
     @_init_console
     def i1(self, msg: str = ''):
         timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S.%f')[:-3]
-        self.logger.debug(f'I/{timestamp} {msg}')
+        self.logger.info(f'I/{timestamp} {msg}')
 
     @_init_console
     def w1(self, msg: str = ''):
         timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S.%f')[:-3]
-        self.logger.debug(f'W/{timestamp} {msg}')
+        self.logger.warning(f'W/{timestamp} {msg}')
 
     @_init_console
     def e1(self, msg: str = ''):
         timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S.%f')[:-3]
-        self.logger.debug(f'E/{timestamp} {msg}')
+        self.logger.error(f'E/{timestamp} {msg}')
 
     @_init_console
     def c1(self, msg: str = ''):
         timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S.%f')[:-3]
-        self.logger.debug(f'C/{timestamp} {msg}')
+        self.logger.critical(f'C/{timestamp} {msg}')
 
     @_init_console_with_level
     def l1(self, level: int, msg: str = ''):
