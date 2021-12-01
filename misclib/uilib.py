be58e6ba13ba340e08829fe738b517bc35fad228
ppcrong <ppcrong@gmail.com>
2021-12-01 11:37:20 +0800

    [uilib] add resize_widget and center apis

--- misclib/uilib.py
+++ misclib/uilib.py
@@ -1,4 +1,10 @@
+from PyQt5.QtWidgets import QWidget, QDesktopWidget
+
+from loglib.loglib import loglib
+
+
 class uilib:
+    slogger = loglib(__name__)
 
     @staticmethod
     def get_text_size(font_name: str, font_size: int, text: str):
@@ -34,3 +40,32 @@ class uilib:
         else:
             ret_text = text
         return ret_text
+
+    @staticmethod
+    def resize_widget(widget: QWidget, ratio_w: float = 1, ratio_h: float = 1, center: bool = True):
+        # resize by ratio fullscreen
+        import PyQt5
+        res = PyQt5.QtWidgets.QDesktopWidget().availableGeometry()
+        uilib.slogger.info(f'full (w,h):({res.width()},{res.height()})')
+        w, h = res.width() * ratio_w, res.height() * ratio_h
+        uilib.slogger.info(f'resize (w,h):({w:.2f},{h: .2f})')
+        widget.setFixedSize(w, h)
+        widget.setMaximumSize(w, h)
+
+        if center:
+            # bring screen to center
+            uilib.center(widget=widget)
+
+    @staticmethod
+    def center(widget: QWidget):
+        # geometry of the main window
+        qr = widget.frameGeometry()
+
+        # center point of screen
+        cp = QDesktopWidget().availableGeometry().center()
+
+        # move rectangle's center point to screen's center point
+        qr.moveCenter(cp)
+
+        # top left of rectangle becomes top left of window centering it
+        widget.move(qr.topLeft())
