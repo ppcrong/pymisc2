29efd9c80151e4e2fee84aa0d5953d16a86b8317
ppcrong <ppcrong@gmail.com>
2021-12-03 18:15:24 +0800

    [serlib] add serial settings tuples

---
+++ serlib/__init__.py
@@ -0,0 +1,5 @@
+UART_SPEED = (110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200, 230400, 460800, 921600)
+UART_DATA = ('7 bit', '8 bit')
+UART_PARITY = ('none', 'odd', 'even', 'mark', 'space')
+UART_STOP_BITS = ('1 bit', '1.5 bit', '2 bit')
+UART_FLOW_CONTROL = ('Xon/Xoff', 'RTS/CTS', 'DSR/DTR', 'none')
