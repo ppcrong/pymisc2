511a6f213801fcf8eed983cef0685b3ed924efe0
ppcrong <ppcrong@gmail.com>
2021-12-07 15:09:53 +0800

    [serlib] use dictionary instead of enum

--- serlib/__init__.py
+++ serlib/__init__.py
@@ -1,60 +1,48 @@
 import enum
 
+from serial import SEVENBITS, EIGHTBITS, PARITY_NONE, PARITY_ODD, PARITY_EVEN, PARITY_MARK, PARITY_SPACE, STOPBITS_ONE, \
+    STOPBITS_ONE_POINT_FIVE, STOPBITS_TWO
 
-class ENUM_BAUDRATE(enum.IntEnum):
-    """
-    baudate
-    """
-    B_110 = 110
-    B_300 = 300
-    B_600 = 600
-    B_1200 = 1200
-    B_2400 = 2400
-    B_4800 = 4800
-    B_9600 = 9600
-    B_14400 = 14400
-    B_19200 = 19200
-    B_38400 = 38400
-    B_57600 = 57600
-    B_115200 = 115200
-    B_230400 = 230400
-    B_460800 = 460800
-    B_921600 = 921600
+BAUDRATES = (
+    '110',
+    '300',
+    '600',
+    '1200',
+    '2400',
+    '4800',
+    '9600',
+    '14400',
+    '19200',
+    '38400',
+    '57600',
+    '115200',
+    '230400',
+    '460800',
+    '921600'
+)
 
+DICT_DATA = {
+    '7 bit': SEVENBITS,
+    '8 bit': EIGHTBITS,
+}
 
-class ENUM_DATA(enum.Enum):
-    """
-    data
-    """
-    BIT_7 = '7 bit'
-    BIT_8 = '8 bit'
+DICT_PARITY = {
+    'none': PARITY_NONE,
+    'odd': PARITY_ODD,
+    'even': PARITY_EVEN,
+    'mark': PARITY_MARK,
+    'space': PARITY_SPACE
+}
 
+DICT_STOP_BITS = {
+    '1 bit': STOPBITS_ONE,
+    '1.5 bit': STOPBITS_ONE_POINT_FIVE,
+    '2 bit': STOPBITS_TWO,
+}
 
-class ENUM_PARITY(enum.Enum):
-    """
-    parity
-    """
-    NONE = 'none'
-    ODD = 'odd'
-    EVEN = 'even'
-    MARK = 'mark'
-    SPACE = 'space'
-
-
-class ENUM_STOP_BITS(enum.Enum):
-    """
-    stop bits
-    """
-    BIT_1 = '1 bit'
-    BIT_1p5 = '1.5 bit'
-    BIT_2 = '2 bit'
-
-
-class ENUM_FLOW_CONTROL(enum.Enum):
-    """
-    parity
-    """
-    XON_XOFF = 'Xon/Xoff'
-    RTS_CTS = 'RTS/CTS'
-    DSR_DTR = 'SDR/DTR'
-    NONE = 'none'
+FLOW_CONTROLS = (
+    'Xon/Xoff',
+    'RTS/CTS',
+    'DSR/DTR',
+    'none'
+)
